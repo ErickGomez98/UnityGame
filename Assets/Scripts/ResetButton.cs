7ed790a14aa2a04418c1a54a1a6de0ac43b21b9b
Erick Gómez <erick0gomez98@gmail.com>
2020-05-25 18:24:11 -0500

    First commit with a lot of stuff

---
+++ Assets/Scripts/ResetButton.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class GameObjectInitialState
+{
+    public int id;
+    public Vector3 position;
+    public Quaternion rotation;
+}
+public class ResetButton : XRBaseInteractable
+{
+   
+
+    private List<GameObjectInitialState> moveableGameObjects = new List<GameObjectInitialState>();
+    protected override void Awake()
+    {
+        base.Awake();
+        onHoverEnter.AddListener(OnButtonPress);
+
+        // Store current position of all objects in game
+        GameObject[] objects = GameObject.FindGameObjectsWithTag("MoveableGameObject");
+        if(objects.Length > 0)
+        {
+            Debug.Log("si hay mas de 1 xd");
+        }
+        foreach (GameObject gameObject in objects)
+        {
+            GameObjectInitialState i = new GameObjectInitialState
+            {
+                id = gameObject.GetInstanceID(),
+                position = gameObject.transform.position,
+                rotation = gameObject.transform.rotation
+            };
+            
+            moveableGameObjects.Add(i);
+        }
+    }
+    private void OnButtonPress(XRBaseInteractor interactor)
+    {
+        GameObject[] currentGameObjects = GameObject.FindGameObjectsWithTag("MoveableGameObject");
+        if (currentGameObjects.Length > 0)
+        {
+            Debug.Log("si hay mas de 12222 xd");
+        }
+        foreach (GameObject gameObject in currentGameObjects)
+        {
+            var storedGameObject = moveableGameObjects.Find(item => item.id == gameObject.GetInstanceID());
+
+            if (storedGameObject != null)
+            {
+                if(storedGameObject.position != gameObject.transform.position && storedGameObject.rotation != gameObject.transform.rotation)
+                {
+                    gameObject.transform.position = storedGameObject.position;
+                    gameObject.transform.rotation = storedGameObject.rotation;
+                }
+
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class StartGame : XRBaseInteractable
+{
+    protected override void Awake()
+    {
+        base.Awake();
+        onHoverEnter.AddListener(OnButtonPress);
+    }
+    private void OnButtonPress(XRBaseInteractor interactor)
+    {
+        Debug.Log("ey start game");
+    }
+}
